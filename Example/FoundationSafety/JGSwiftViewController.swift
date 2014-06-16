da209334acd6d883f419c190adcda9677e67a167
Jai Govindani <govindani@gmail.com>
2014-06-16 23:07:35 +0700

    Add Swift bridging header
    Signed-off-by: Jai Govindani <govindani@gmail.com>

---
+++ Example/FoundationSafety/JGSwiftViewController.swift
@@ -0,0 +1,9 @@
+//
+//  JGSwiftViewController.swift
+//  FoundationSafety
+//
+//  Created by Jai Govindani on 6/14/14.
+//  Copyright (c) 2014 Jai Govindani. All rights reserved.
+//
+
+import Foundation
