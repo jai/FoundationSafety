cf3e14e1c6adde89e1d68930059e6083cec2ecb6
Jai Govindani <govindani@gmail.com>
2014-06-22 23:40:09 +0700

    Add documentation for NSMutableArray+Safety
    Signed-off-by: Jai Govindani <govindani@gmail.com>

--- Classes/ios/NSMutableArray+Safety/NSMutableArray+Safety.h
+++ Classes/ios/NSMutableArray+Safety/NSMutableArray+Safety.h
@@ -10,6 +10,11 @@
 
 @interface NSMutableArray (Safety)
 
+/**
+ *  Checks if an object is nil before adding to the array, in order to avoid an exception.
+ *
+ *  @param object The object to add. Can be nil, this method will check first before adding.
+ */
 - (void)safeAddObject:(id)object;
 
 @end
